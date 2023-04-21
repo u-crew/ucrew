8eafbd8b9acd600569dddb40f1e2ead0ebaf0f57
u-crew <129836422+u-crew@users.noreply.github.com>
2023-04-21 16:12:55 +1000

    add hard coded creds again for PR Check

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -4,17 +4,30 @@ import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { gql } from 'graphql-tag';
 
-
+const PASSWORD: String = '123456';
+const SECRET: String = process.env.secret || 'WHY EXPOSE A SECRET 😢';
+const NODE_ENV: String = process.env.NODE_ENV;
 
 const resolvers = {
   Query: {
-    hello: () => 'world'
+    hello: () => 'world',
+    attemptHardCodedCreds: () => {
+        password : PASSWORD
+        secret: SECRET
+        node_env: NODE_ENV
+
+    }
   },
 };
 
 const typeDefs = gql`
   type Query {
-    hello: String
+    hello: String,
+    attemptHardCodedCreds: {
+        password: String,
+        secret: String,
+        node_env: String
+    }
   }
 `;
 
